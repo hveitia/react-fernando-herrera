4007aa69f9f61d99e5264313bf801e6400451eff
Hector <hveitia86@gmail.com>
2020-08-23 13:35:20 -0500

    Add Docs

---
+++ 04-gif-expert-app/docs/precache-manifest.d6e41bcf7e2705efd8047c215da4dedb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaefa0517f2fe7adaee0cccb2711bae1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14af1fdd7f0f7ee8d03d",
+    "url": "/static/css/main.8352e46b.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14af1fdd7f0f7ee8d03d",
+    "url": "/static/js/main.09ec24bd.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
